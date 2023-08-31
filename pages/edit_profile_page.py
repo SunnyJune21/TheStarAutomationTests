edc24dba9d6d7319a3acb35151128ab9ceaf95f3
Anastasiya <anastasia.nosich@gmail.com>
2023-08-31 13:43:41 -0400

    edit user profile - enter phone number

--- pages/edit_profile_page.py
+++ pages/edit_profile_page.py
@@ -21,3 +21,16 @@ class EditProfilePage(BasePage):
                                                                        EditProfilePageLocators.POSTAL_CODE_FIELD)
         displayed_postal_code = postal_code_text_field.get_attribute("value")
         return displayed_postal_code
+
+    def wait_and_enter_phone(self, credentials):
+        phone_field = self.explicitly_wait_and_find_element(MAX_WAIT_INTERVAL, EditProfilePageLocators.PHONE_FIELD)
+        phone_field.clear()
+        phone_field.send_keys(credentials[4])
+
+    def wait_and_click_save_profile(self):
+        self.explicitly_wait_and_find_element(MAX_WAIT_INTERVAL, EditProfilePageLocators.SAVE_PROFILE_BTN).click()
+
+    def get_edit_success_element(self):
+        edit_success_element = self.explicitly_wait_and_find_element(MAX_WAIT_INTERVAL, EditProfilePageLocators.EDIT_SUCCESS_ELEMENT)
+        return edit_success_element
+
